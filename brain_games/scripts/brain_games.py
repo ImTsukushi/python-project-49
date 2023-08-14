d97fc48eaa15821ee028da882e1f03f4e7687957
ImTsukushi <cucker30000@gmail.com>
2023-08-14 15:01:15 +0300

    add greetings

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
+import brain_games
+
 
 def greet():
     print("Welcome to the Brain Games!")
 
+
 def main():
     greet()
+    brain_games.welcome_user()
+
 
 if __name__ == '__main__':
     main()
