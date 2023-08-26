a07563418361dd1e3fad8914e7e4f35c69cb22ef
ImTsukushi <cucker30000@gmail.com>
2023-08-26 11:10:32 +0300

    refactoring progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,18 +1,43 @@
 from random import randint
 
 TASK = 'What number is missing in the progression?'
+NUM_VALUES_IN_PROGRESSION = 10
+MIN_FIRST_NUMBER = 1
+MAX_FIRST_NUMBER = 20
+MIN_PROGRESSION_STEP = 2
+MAX_PROGRESSION_STEP = 5
+MIN_HIDDEN_INDEX = 0
+MAX_HIDDEN_INDEX = 9
 
 
-def get_game_data():
-    first_number = randint(1, 20)
-    progression_step = randint(2, 5)
-    hidden_number = randint(0, 9)
+# Создаем числа для прогрессии
+def create_progression_data():
+    initial_value = randint(MIN_FIRST_NUMBER, MAX_FIRST_NUMBER)
+    progression_step = randint(MIN_PROGRESSION_STEP, MAX_PROGRESSION_STEP)
+    hidden_index = randint(MIN_HIDDEN_INDEX, MAX_HIDDEN_INDEX)
+    return initial_value, progression_step, hidden_index
+
+
+# Создаем прогрессию длинной 10 чисел
+def make_progression_list(first_number, progression_step):
     progression = []
-    # Создаем прогрессию длинной 10 чисел
-    for i in range(10):
+    for i in range(NUM_VALUES_IN_PROGRESSION):
         progression.append(str(first_number))
         first_number += progression_step
+    return progression
+
+
+# Вычисляем правильный ответ
+def find_correct_answer(progression, hidden_number):
     correct_answer = progression[hidden_number]
     progression[hidden_number] = '..'
     question = ' '.join(progression)
+    return correct_answer, question
+
+
+# Игра
+def get_game_data():
+    first_number, progression_step, hidden_number = create_progression_data()
+    progression = make_progression_list(first_number, progression_step)
+    correct_answer, question = find_correct_answer(progression, hidden_number)
     return correct_answer, question
