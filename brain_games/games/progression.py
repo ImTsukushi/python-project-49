b25eb4fd4aba56f024cf7aefe75f367e3b7a4201
ImTsukushi <cucker30000@gmail.com>
2023-09-03 11:17:40 +0300

    upgrade progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -10,31 +10,29 @@ MIN_HIDDEN_INDEX = 0
 MAX_HIDDEN_INDEX = 9
 
 
-# Создаем числа для прогрессии
-def create_progression_data():
-    initial_value = randint(MIN_FIRST_NUMBER, MAX_FIRST_NUMBER)
-    progression_step = randint(MIN_PROGRESSION_STEP, MAX_PROGRESSION_STEP)
-    hidden_index = randint(MIN_HIDDEN_INDEX, MAX_HIDDEN_INDEX)
-    return initial_value, progression_step, hidden_index
-
-
-# Создаем прогрессию длинной 10 чисел, выбираем скрываемое число
-def make_progression_list(first_number, progression_step, hidden_index):
+# Создаем прогрессию длинной 10 чисел
+def make_progression(first_number, progression_step):
     progression = []
     for i in range(LENGTH):
         progression.append(str(first_number))
         first_number += progression_step
+    return progression
+
+
+# Скрываем правильный ответ в прогрессии
+def make_hidden_number(progression):
+    hidden_index = randint(MIN_HIDDEN_INDEX, MAX_HIDDEN_INDEX)
     hidden_number = progression[hidden_index]
     progression[hidden_index] = '..'
-    return progression, hidden_number
+    return hidden_number, progression
 
 
-# Вычисляем правильный ответ
+# Генерируем числа, создаем вопрос, вычисляем правильный ответ
 def get_game_data():
-    first_number, progression_step, hidden_index = create_progression_data()
-    progression, hidden_number = make_progression_list(
-        first_number, progression_step, hidden_index
-    )
+    first_number = randint(MIN_FIRST_NUMBER, MAX_FIRST_NUMBER)
+    progression_step = randint(MIN_PROGRESSION_STEP, MAX_PROGRESSION_STEP)
+    progression = make_progression(first_number, progression_step)
+    hidden_number, progression = make_hidden_number(progression)
     correct_answer = hidden_number
     question = ' '.join(progression)
     return correct_answer, question
